2058f13dfa45e12fbf75af07aba9728d2063f48d
URim Shin <yoolim33no1@gmail.com>
2023-12-02 19:00:37 +0900

    Change

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -1,30 +1,29 @@
 package kr.ac.hansung;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java Fibonacci <n>");
-            return;
+        public static int fibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
+        public static void main(String[] args) {
+            if (args.length != 1) {
+                System.out.println("Usage: java Fibonacci <n>");
+                return;
+            }
 
-        int n = Integer.parseInt(args[0]);
-        if (n < 0) {
-            System.out.println("Please provide a non-negative integer.");
-            return;
-        }
+            int n = Integer.parseInt(args[0]);
+            if (n < 0) {
+                System.out.println("Please provide a non-negative integer.");
+                return;
+            }
 
-        for (int i = 0; i < n; i++) {
-            //System.out.print(fibonacci(i) + " ");
-            int fib=fibonacci(i);
-            System.out.print("fibonacci("+i+")="+fib);
-            SYstem.out.println();
-        }
-    }
-
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+            for (int i = 0; i < n; i++) {
+                //System.out.print(fibonacci(i) + " ");
+                int fib=fibonacci(i);
+                System.out.print("fibonacci("+i+")="+fib);
+                SYstem.out.println();
+            }
     }
 }
